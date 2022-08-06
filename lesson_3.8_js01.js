5a451107ec43dbac22744222ccd16d4210a68f4f
VHVSK <vygovskiy.v@gmail.com>
2022-08-06 16:31:57 +0300

    Update

---
+++ lesson_3.8_js01.js
@@ -0,0 +1,15 @@
+let myName
+console.log(myName)
+// undefined
+
+myName = 'Bogdan'
+
+console.log(myName)
+// undefined
+// Bogdan
+
+// в терміналі не повертається undefined після Bogdan
+// Якщо потрібно
+console.log(console.log(10))
+// 10
+// undefined
---
+++ lesson_3.8_js02.js
@@ -0,0 +1,7 @@
+const myName = 'Bogdan'
+
+// Якщо спробувати змінити константу.
+// myName = '' // TypeError: Assignment to constant variable.
+// Якщо змінюбвати код, то виклонання коду зупиняється
+
+console.log(myName)
