47b1e931e9b78f2e896f5805b89ee48bbccedefd
VHVSK <vygovskiy.v@gmail.com>
2022-09-02 17:09:03 +0300

    Оновлення

---
+++ TASKS/01-variable-declaration/finish.js
@@ -0,0 +1,17 @@
+/** ЗАДАЧА 1 - Объявление переменной
+ *
+ * 1. Объявите переменную "myCountry" и присвойте ей значение -
+ * строку с вашей страной
+ *
+ * 2. В дальнейшем не планируется присваивание нового значения этой переменной
+ *
+ * 3. Выведите значение переменной в консоль
+ */
+
+const myCountry = 'Ukraine'
+console.log(myCountry)
+
+// [Running] node "c:\OpenServer\domains\myJS\TASKS\01-variable-declaration\finish.js"
+// Ukraine
+
+// [Done] exited with code=0 in 0.081 seconds
