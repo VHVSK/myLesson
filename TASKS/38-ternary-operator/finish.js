1e8b90b31c9e713dc16cd148303dd057b4afd938
VHVSK <vygovskiy.v@gmail.com>
2022-11-14 20:10:01 +0200

    Оновлення

--- TASKS/38-ternary-operator/finish.js
+++ TASKS/38-ternary-operator/finish.js
@@ -7,13 +7,26 @@
  * 3. Замените обычную функцию на стрелочную функцию
  */
 
-function isArrayEmpty(inputArray) {
-  if (inputArray.length > 0) {
-    return 'Массив не пустой'
-  } else {
-    return 'Массив пустой'
-  }
+// function isArrayEmpty(inputArray) {
+//   if (inputArray.length > 0) {
+//     return 'Массив не пустой'
+//   } else {
+//     return 'Массив пустой'
+//   }
+// }
+
+const isArrayEmpty = (inputArray) => {
+  return inputArray.length > 0 ? 'Массив не пустой' : 'Массив пустой'
 }
 
 console.log(isArrayEmpty([1, 3]))
 console.log(isArrayEmpty([]))
+
+// блін
+// не потріно було {}
+// return - прериває виконання функції, тому else не потріно!
+// правильно та:
+/**
+ * const isArrayEmpty = (inputArray) =>
+  inputArray.length > 0 ? 'Массив не пустой' : 'Массив пустой'
+ */
--- TASKS/39-foreach-with-index/finish.js
+++ TASKS/39-foreach-with-index/finish.js
@@ -10,3 +10,30 @@
  */
 
 const myCities = ['London', 'New York', 'Singapore']
+
+function cityInfo (city, indexArr) {
+    console.log(city + ' is at the index ' + indexArr + ' in the myCities array')
+}
+
+myCities.forEach(cityInfo)
+
+/**
+ * правильний варіант
+ *  const myCities = ['London', 'New York', 'Singapore']
+
+    const cityInfo = (city, index) =>
+    `${city} is at the index ${index} in the myCities array`
+
+    myCities.forEach((city, index) => console.log(cityInfo(city, index)))
+
+    Якщо ми додамо фігурні дужки (відбувається деструктуризація об'єкту), то ми поредаємо об'єкт, в такому випадку ми можемо змінювати порядок змінник, так як в об'єкті немає різниці який порядок. 
+
+    const myCities = ['London', 'New York', 'Singapore']
+
+    const cityInfo = ({city, index}) =>
+    `${city} is at the index ${index} in the myCities array`
+
+    myCities.forEach((city, index) => console.log(cityInfo({index, city})))
+
+    ... cityInfo({index, city}))) - це ніби такий запис: cityInfo({index: index, city: city})))
+ */
