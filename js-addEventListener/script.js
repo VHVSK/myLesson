45f7e97d37ed99513ae3f6c04ec42c38b82a47e2
VHVSK <vygovskiy.v@gmail.com>
2023-01-23 19:07:49 +0200

    1

--- js-addEventListener/script.js
+++ js-addEventListener/script.js
@@ -2,11 +2,11 @@
 // 1. Прописати в самому коді: <button onclick="console.log('Клік')">Ловимо подію кліка</button>
 // 2. Використання DOM об'єкта
 const button = document.querySelector('.button')
-button.onclick = () => alert('Клік, зловили через DOM (стрілочна функція)')
+button.onclick = () => alert('Клік, зловили через DOM (стрілочна функція =>)')
 
 // можна запакувати у функцію
-const button2 = document.querySelector('.button2')
-button2.onclick = myFunction
+const button_2 = document.querySelector('.button_2')
+button_2.onclick = myFunction
 // Щоб не визвати автоматичне виконання цієї функції, ми пишемо без дужок!!!
 
 function myFunction() {
@@ -14,3 +14,34 @@ function myFunction() {
 }
 
 // недолік ціих методів - не можна повісити декілька методів на одну подію, так як це перезапише подію
+// основний метод реагувати на подію, це методи addEventListener та removeEventListener
+const button_3 = document.querySelector('.button_3')
+button_3.addEventListener('click', function (e) {
+  alert('Клік, через функцію addEventListener')
+})
+button_3.addEventListener('click', function (e) {
+  alert('Клік, через функцію addEventListener 2')
+})
+
+// Можгна винести код функції без назви у функцію з назвою
+button_3.addEventListener('click', testAlert)
+
+function testAlert() {
+  alert('Клік, через функцію addEventListener, через окрему функцію')
+}
+
+// removeEventListener якщо ми не хочемо щоб браузер і надалі слідкував цю подію,
+// це також навантажує систему коритсувача,
+// наприклад:
+const button_4 = document.querySelector('.button_4')
+
+function testAlertRemovEL() {
+  alert(
+    'Клік, через функцію addEventListener, через окрему функцію. Другий клік вже не спрацює!'
+  )
+  button_4.removeEventListener('click', testAlertRemovEL)
+}
+
+button_4.addEventListener('click', testAlertRemovEL)
+
+// 7-31 Параметри
