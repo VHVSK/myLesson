e8a24861b00aaafd875a053c3cbb5248c4ecfa1c
VHVSK <vygovskiy.v@gmail.com>
2023-02-03 18:20:28 +0200

    Оновлення

---
+++ React-myLesson_BogdanS/07-JSX-test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*#__PURE__*/ React.createElement(
+  'div',
+  null,
+  /*#__PURE__*/ React.createElement('h1', null, 'Hello'),
+  /*#__PURE__*/ React.createElement(
+    'div',
+    null,
+    /*#__PURE__*/ React.createElement('h2', null, 'Hello')
+  )
+)
+
+/* <div>
+        <h1>Hello</h1>
+        <div>
+            <h2>Hello</h2>
+        </div>
+    </div> */
+
+// https://babeljs.io/
