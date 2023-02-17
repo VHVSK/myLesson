999b9c99d9d1204fa169f6b7dcb8267be3d771bf
VHVSK <vygovskiy.v@gmail.com>
2023-02-17 20:23:59 +0200

    U

---
+++ React-myLesson_BogdanS/13-reusable-components/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello from the reusable components</h1>
+      <button>Like!</button>
+    </div>
+  )
+}
+
+function App() {
+  return <div className="App"></div>
+}
+
+export default App
