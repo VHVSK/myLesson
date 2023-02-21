4bba8a1001ec4c82f1de110f301b84115d537a85
VHVSK <vygovskiy.v@gmail.com>
2023-02-21 21:22:21 +0200

    U

--- React-myLesson_BogdanS/16-state/src/App.js
+++ React-myLesson_BogdanS/16-state/src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import RandomNamber from './components/RandomNamber'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <RandomNamber />
+    </div>
+  )
 }
 
 export default App
