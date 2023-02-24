b24d3fbef72d7770a769d84814f5d14231609309
VHVSK <vygovskiy.v@gmail.com>
2023-02-24 21:42:07 +0200

    U

--- React-myLesson_BogdanS/16-state/src/App.js
+++ React-myLesson_BogdanS/16-state/src/App.js
@@ -10,3 +10,20 @@ function App() {
 }
 
 export default App
+
+// Створили компонент RandomNumber
+// Долдали його в компонент App
+// Длодали стилі в файли index.css and App.css
+
+/** Маємо:
+ * <div id="root">
+ *  <div class="App">
+ *    <div>
+ *      <h1>100</h1>
+ *      <button>Generate new random number</button>
+ *    </div>
+ *  </div>
+ * </div>
+ */
+
+// Далі в файлі RandomNumber.js
