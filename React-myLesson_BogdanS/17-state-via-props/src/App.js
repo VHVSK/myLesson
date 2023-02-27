fde65fb5cb0980f51e35df8b095aabe7ff85e2dc
VHVSK <vygovskiy.v@gmail.com>
2023-02-27 18:57:26 +0200

    U

--- React-myLesson_BogdanS/17-state-via-props/src/App.js
+++ React-myLesson_BogdanS/17-state-via-props/src/App.js
@@ -1,9 +1,25 @@
+import { useState } from React
 import './App.css'
+import Counter from './components/Counter'
+import Button from './components/Button'
 
 // В цьому проекті, передача стану іншому компоненту через властивість
-
 function App() {
-  return <div className="App"></div>
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <Counter count={count} />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  )
 }
 
 export default App
+
+// Виходить, що стан/значення потрібно зберегати в батьківському компоненту для Counter і Button, а це в нас компонетт App
+// Щоб додати стан, ми імпортуємо зовнішній в React useState, зовнішні компоненти ми пишемо з самого початку, а потім вже внутрішні.
+// Далі: const [count, setCount] = useState(0), загально прийняте правило, змінну та функцію називати однаково, тільки з префіксом set
+
