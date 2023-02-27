f0957195dd1f8eac8474e260489e4d38a6e9bdcb
VHVSK <vygovskiy.v@gmail.com>
2023-02-27 21:53:47 +0200

    U

--- React-myLesson_BogdanS/17-state-via-props/src/App.js
+++ React-myLesson_BogdanS/17-state-via-props/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from React
+import { useState } from 'react'
 import './App.css'
 import Counter from './components/Counter'
 import Button from './components/Button'
@@ -6,20 +6,39 @@ import Button from './components/Button'
 // В цьому проекті, передача стану іншому компоненту через властивість
 function App() {
   const [count, setCount] = useState(0)
+  const incrementCount = () => {
+    setCount(count + 1)
+    // console.log(count) // на цьому єтапі ще старе значення
+  }
+  // console.log(count) // на цьому єтапі ВЖЕ НОВЕ значення, це говорить про те, що ОНОВЛЕННЯ ВІДБУВАЮТЬСЯ АСИНХРОННО
+
   return (
     <div className="App">
       <Counter count={count} />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      {/* <Button count={count} onClickFunction={setCount} /> було */}
+      <Button onClickFunction={incrementCount} />
+      <Button onClickFunction={incrementCount} />
+      <Button onClickFunction={incrementCount} />
+      <Button onClickFunction={incrementCount} />
     </div>
   )
 }
 
 export default App
 
-// Виходить, що стан/значення потрібно зберегати в батьківському компоненту для Counter і Button, а це в нас компонетт App
+// Виходить, що стан (значення) потрібно зберегати в батьківському компоненту для Counter і Button, а це в нас компонетт App
 // Щоб додати стан, ми імпортуємо зовнішній в React useState, зовнішні компоненти ми пишемо з самого початку, а потім вже внутрішні.
 // Далі: const [count, setCount] = useState(0), загально прийняте правило, змінну та функцію називати однаково, тільки з префіксом set
 
+// До Counter додали count={count}, а в самому файлі відразу деструктуризували аргументи { count }, та додали в {count} <h1>Total clicks: {count}</h1>
+// До Button додали: <Button count={count} onClickFunction={setCount} />
+// та в Button.js відрузу деструктуризували параметри { count, onClickFunction }
+// а також додали: onClick={() => onClickFunction(count + 1)}
+// (цей абзац ми переписали на наступний)
+
+// Але, такий варіант не рекомендується, кнопкам не обо'вязково знати стан батьківського елемента, тому нам потрібно цзолювати станн від кнопок. Щоб це зробити, нам потрібно сткорити фонкцію в App, яка буде приймати стан та збільшувати його, а кнопки будуть лише визивати цю функцію.
+// Тобто, ми можемо впливати на батькіський компонент з дочірнього!
+// Також, виносити цю функцію з функції App не потрібно, так як вона безпосередньо впливає на цей компонент App
+// в Button.js ми видалии count={count}, а також переписали onClick={onClickFunction}
+
+// Далі ми перевірили черговіть виконання, також виявили таку властивість, що у ВІРТУАЛЬНОМУ ДОМ оновлюються всі дочерні елементи (коспоненти), а в РЕАЛЬНОМУ ДОМ тільки змінються значення (те що змінилося, в даному випадку число яке вказує на кількість кліків.)
--- React-myLesson_BogdanS/17-state-via-props/src/components/Button.js
+++ React-myLesson_BogdanS/17-state-via-props/src/components/Button.js
@@ -1,5 +1,8 @@
-function Button() {
-  return <button>Click me!</button>
+function Button({ onClickFunction }) {
+  return <button onClick={onClickFunction}>Click me!</button>
 }
 
 export default Button
+
+// в даному випадку ми передали назву функції в змінній onClickFunction, а згначення це incrementCount.
+// Виходить визивається функція incrementCount
