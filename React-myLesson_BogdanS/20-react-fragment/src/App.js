049b40cda5c2729c18466f1e839ddf0bf46c64ef
VHVSK <vygovskiy.v@gmail.com>
2023-03-03 20:33:38 +0200

    U

---
+++ React-myLesson_BogdanS/20-react-fragment/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  )
+}
+
+export default App
+
+// В двному уроці ми розберемося що таке Fragment, це дасть нам можливість додлвати компоненти без датьківського тегу
